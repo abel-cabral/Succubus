d41f073217fde73e3e2c538af7e027c3cc2a0d07
Abel Cabral de Arruda <abel@ibm.com>
2020-11-01 19:05:46 -0300

    removed old telegram library

--- src/bot.js
+++ src/bot.js
@@ -1,16 +1,36 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const Telegraf = require('telegraf')
+const Telegraf = require('telegraf');
+const Markup = require('telegraf/markup');
+const Extra = require('telegraf/extra');
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
+const moment = require('moment');
+moment.locale('pt-br');
+//moment().format('LTS');
+
+// each time that hear something, will execute it
 bot.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const response_time = new Date() - start
-  const chat_from = `${ctx.message.chat.first_name} (id: ${ctx.message.chat.id})`
-  console.log(`Chat from ${chat_from} (Response Time: ${response_time})`)
-})
+  const start = new Date();
+  await next();
+  const response_time = new Date() - start;
+  // const chat_from = `${ctx.message.chat.first_name} (id: ${ctx.message.chat.id})`
+  // console.log(`Chat from ${chat_from} (Response Time: ${response_time})`)
+});
 
-bot.hears('Assalamualaikum', (ctx) => ctx.reply('Waalaikumsalam'))
-bot.launch()
+bot.hears('hello', (ctx) => {
+  ctx.reply(
+    '<b>Hello</b>. <i>How are you today?</i>',
+    Extra.HTML().markup(
+      Markup.inlineKeyboard([
+        Markup.callbackButton('Not bad', 'not bad'),
+        Markup.callbackButton('All right', 'all right'),
+      ]),
+    ),
+  );
+});
+
+bot.hears('Assalamualaikum', (ctx) => ctx.reply('Waalaikumsalam'));
+
+bot.launch();
